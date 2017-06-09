d103bcaf3e21dac54686d0c3da21c1333a2c7b02
Jeremy Kun <jeremy@21.co>
2017-06-08 18:02:35 -0700

    main: add rotation slider

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as d3 from 'd3';
 import { Vector, Ellipse } from './geometry';
 
 let width = 800;
-let height = 600;
+let height = 800;
 let svg = d3.select("body").insert("svg", ":first-child")
                            .attr("width", width)
                            .attr("height", height);
@@ -13,7 +13,9 @@ let svg = d3.select("body").insert("svg", ":first-child")
 let unit = 60;  // unit to pixel conversion
 let originX = width / 2;
 let originY = height / 2;
-let origin = Vector(originX, originY);
+let origin = new Vector(originX, originY);
+let fill = 'rgb(120,33,33)';
+let strokeColor = 'black';
 function fromCartesianX(x) { return originX + x; }
 function fromCartesianY(y) { return originY - y; }
 function toCartesianX(x) { return x - originX; }
@@ -30,14 +32,16 @@ function createEllipseSVG(ellipse) {
 
 function setupEllipseStyle(ellipseSVG) {
   let {ellipse} = ellipseSVG;
-  ellipse.attr("fill", function (d) { return d.fill; })
-         .attr("stroke", function (d) { return d.strokeColor; })
+  ellipse.attr("fill", function (d) { return fill; })
+         .attr("stroke", function (d) { return strokeColor; })
          .attr("stroke-width", 2)
-         .id("ellipse");
+         .attr("fill-opacity", 0.3)
+         .attr("id", "ellipse");
 }
 
-function setupBehavior(ellipseSVG) {
+function setupBehavior(ellipseSVG, sliders) {
   let {ellipse} = ellipseSVG;
+  let {rotationSlider} = sliders;
 
   function setEllipsePosition(svg) {
     svg.attr("cx", function(d) { return fromCartesianX(d.center.x); })
@@ -46,9 +50,11 @@ function setupBehavior(ellipseSVG) {
        .attr("ry", function(d) { return d.minorAxisLength; });
 
     svg.attr('transform', function(d) {
-      let rotationFromHorizontal = d.rotationAngle;
-      let rotationFromVeritcal = (Math.PI / 2) - rotationFromHorizontal;
-      return ("rotate(" + rotationFromVertical + ")");
+      let angleDegrees = (-d.rotationAngle) * (180 / Math.PI);
+      let center = d.center;
+      let centerX = fromCartesianX(center.x);
+      let centerY = fromCartesianY(center.y);
+      return ("rotate(" + angleDegrees + " " + centerX + " " + centerY + ")");
     });
   }
 
@@ -58,11 +64,27 @@ function setupBehavior(ellipseSVG) {
     // update svgs
   }
 
+  rotationSlider.on("input", function() {
+    let value = +this.value;
+    let angleRadians = value * Math.PI / 180;
+    ellipse.datum().rotationAngle = angleRadians;
+    setEllipsePosition(ellipse);
+  })
+
   // ellipse.call(d3.drag().on("drag", dragged));
   setEllipsePosition(ellipse);
 }
 
-let e1 = new Ellipse(origin, 2 * unit, unit);
+function createSliders() {
+  let rotationSlider = d3.select('#rotationAngle');
+  return {
+    rotationSlider: rotationSlider
+  };
+}
+
+let e1 = new Ellipse(new Vector(0, 0), 2 * unit, unit, Math.PI / 6);
 let ellipseSVG = createEllipseSVG(e1);
 setupEllipseStyle(ellipseSVG);
-setupBehavior(ellipseSVG);
+
+let sliders = createSliders();
+setupBehavior(ellipseSVG, sliders);
